0dd6ad5ee5d00ae3f60608d9799e0b1dc8dc2939
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-27 20:01:19 -0600

    Wrote the file for HardwareSoftware, but I can't run anything because Mac is stupid and will not allow me to install python 2.7 AT ALL. Searched stackoverflow and google for hours, not a single solution out there.

--- brain.js/HardwareSoftware/app.ts
+++ brain.js/HardwareSoftware/app.ts
@@ -1,1 +1,17 @@
-// let's boogey!
+const brain = require('brain.js');
+const data = require('./data.json');
+
+const network = new brain.recurrent.LSTM();
+
+const trainingData: string[] = data.map(item => ({
+  input: item.text,
+  output: item.category
+}));
+
+network.train(trainingData, {
+  iterations: 2000
+});
+
+const output = network.run('buy charging block');
+
+console.log(`Category: ${output}`);
